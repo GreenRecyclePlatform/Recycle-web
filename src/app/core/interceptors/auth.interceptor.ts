e2b72caa826d2470d258814f98f2b4b8558c61fa
Mohammed-Hassan <158804620+Mohammed-Bata@users.noreply.github.com>
2025-11-29 21:06:42 +0200

    Update auth.interceptor.ts

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,35 +1,63 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import { TokenService } from '../services/tokenservice';
+import { AuthService } from '../services/authservice';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
+import { Token } from '@angular/compiler';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
+  const authService = inject(AuthService);
+  const tokenService = inject(TokenService);
+  const accessToken = tokenService.getToken();
 
-  // Get token from localStorage
-  const token = localStorage.getItem('token') || sessionStorage.getItem('token');
-
-  // Clone request and add authorization header if token exists
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  if (req.url.includes('/Users/Login') || req.url.includes('/Users/Register')) {
     return next(req);
   }
 
+  if (accessToken) {
+    const clonedRequest = addTokenToRequest(req, accessToken);
+    return next(clonedRequest);
+  }
+
   return next(req).pipe(
-    catchError((error) => {
+    catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
-        // Unauthorized - clear tokens and redirect to login
-        localStorage.removeItem('token');
-        localStorage.removeItem('userId');
-        router.navigate(['/login']);
+        return handle401Error(req, next, tokenService, authService);
       }
       return throwError(() => error);
     })
   );
 };
 
+function addTokenToRequest(request: any, accessToken: string) {
+  return request.clone({
+    setHeaders: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    withCredentials: true,
+  });
+}
 
+function handle401Error(
+  request: HttpRequest<any>,
+  next: any,
+  tokenService: TokenService,
+  authService: AuthService
+): Observable<any> {
+  return authService.refresh().pipe(
+    switchMap((loginResponse) => {
+      tokenService.setToken(loginResponse.accessToken);
+      return next(addTokenToRequest(request, loginResponse.accessToken));
+    }),
+    catchError((error) => {
+      authService.logout();
+      return throwError(() => error);
+    })
+  );
+}
