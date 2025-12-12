9b921d5eb387507431fdd52179bd32bbb77b2eb9
MahitabAhmed <mahitab.mohamed2714@gmail.com>
2025-12-12 20:17:52 +0200

    adit notification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -53,7 +53,6 @@ export const routes: Routes = [
   {
     path: 'driver',
     children: [
-
       { path: 'DashBoardDrivers', component: DriverDashboard },
       { path: 'DriverProfile', component: Profiledriver },
     ],
@@ -65,8 +64,7 @@ export const routes: Routes = [
   { path: 'forgot-password', component: ForgotPasswordPage },
   { path: 'reset-password', component: ResetPassword },
 
-  
-{
+  {
     path: 'supplier',
     children: supplierRoutes,
     // Add Auth Guard here if needed
@@ -100,15 +98,14 @@ export const routes: Routes = [
         (m) => m.AdminLayoutComponent
       ),
     children: [
-
-   { path: '',redirectTo: 'dashboard', pathMatch: 'full'},
-       {
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      {
         path: 'dashboard',
-        component: AdminDashboardComponent, 
-        data: { title: 'Dashboard' }
+        component: AdminDashboardComponent,
+        data: { title: 'Dashboard' },
       },
 
-           { path: 'drivers', component: AllDrivers },
+      { path: 'drivers', component: AllDrivers },
       { path: 'assign-drivers', component: AssignDriver },
       { path: 'manage-materials', component: ManageMaterials },
       { path: 'review-requests', component: ReviewRequests },
